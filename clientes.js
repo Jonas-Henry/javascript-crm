9d7f7c816757d9e5793e2059dffae4e9f3bca46e
Jonas-Henry <marceloursulino40@gmail.com>
2025-05-08 16:34:07 -0300

    requisição e resposta

--- clientes.js
+++ clientes.js
@@ -1,5 +1,5 @@
 
-let clientes = sessionStorage.getItem("clientes") ? JSON.parse(sessionStorage.getItem("clientes")) : [];
+let clientes =  [];
 
 function buscarClientes() {
     fetch("http://localhost:3000/clientes")
@@ -9,6 +9,7 @@ function buscarClientes() {
         carregarClientes(clientes);
     })
 }
+
 buscarClientes();
 
 function carregarClientes(listaDeClientes) {
@@ -17,13 +18,14 @@ function carregarClientes(listaDeClientes) {
     listaDeClientes.map((cliente) => {
         tbodyElement.innerHTML += `
         <tr class="*:leading-[40px]">
+                        <td>${cliente.id}</td>
                         <td>${cliente.nome}</td>
                         <td>${cliente.email}</td>
                         <td>${cliente.telefone}</td>
                         <td>${cliente.data}</td>
                         <td class="w-[100px] flex justify-center gap-4">
                             <box-icon name="pencil"></box-icon>
-                            <box-icon name="trash"></box-icon>
+                            <box-icon name="trash" onclick="deletarCliente('${cliente.id}')"></box-icon>
                         </td>
                     </tr>`;
     }
@@ -42,8 +44,29 @@ function cadastrarCliente(form) {
 
     //inserir o cliente no array de clientes
 
-    clientes.push(cliente);
-    sessionStorage.setItem("clientes", JSON.stringify(clientes));
-    mostrarOverlay();
-    carregarClientes(clientes);
+    fetch("http://localhost:3000/clientes", {
+        method: "POST",
+        headers: {
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify(cliente)
+    })
+        .then(res => res.json())
+        .then(() => {
+            alert("Registro cadastrado com sucesso!")
+            mostrarOverlay();
+            carregarClientes(clientes)
+        })
 }
+
+function deletarCliente(id) {
+    fetch(`http://localhost:3000/clientes/${id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(() => {
+        alert(`Item ${id} apagado!`)
+    }
+    )
+}
+
